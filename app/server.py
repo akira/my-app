3ee1f788fbb0db734d8424ed0b8c0346189652e4
Alex Kira <alex.kira@gmail.com>
2023-12-05 18:09:50 -0800

    Add rag conv

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 
+
 app = FastAPI()
 
 
@@ -12,8 +13,10 @@ async def redirect_root_to_docs():
 
 # Edit this to add the chain you want to add
 from pirate_speak.chain import chain as pirate_speak_chain
+from rag_conversation import chain as rag_conversation_chain
 
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
+add_routes(app, rag_conversation_chain, path="/rag-conversation")
 
 if __name__ == "__main__":
     import uvicorn
